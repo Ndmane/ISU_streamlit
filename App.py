f0d16fe533aa6ff07101ef8a0568d9f422d70f0e
Ndmane <132597001+Ndmane@users.noreply.github.com>
2024-12-11 03:16:44 +0500

    Update App.py

--- App.py
+++ App.py
@@ -8,38 +8,43 @@ import numpy as np
 image = Image.open('img/Girl.jpg')
 st.set_page_config(initial_sidebar_state="collapsed", page_icon=image)
 
-pages = ["Home", "Project1", "Project2", "Project3"]
+pages = ['Home', 'Project1', 'Project2', 'Project3']
 
 styles = {
     "nav": {
-        "background-color": "rgb(123, 209, 146)",
+        "background-color": "lightgray",
+        "display": "flex",
+        "justify-content": "center"
     },
-    "div": {
-        "max-width": "32rem",
+    "img": {
+        "position": "absolute",
+        "left": "200px",
+        "top": "1px",
+        "width": "300px",
+        "height": "45px",
     },
     "span": {
-        "border-radius": "0.5rem",
-        "color": "rgb(49, 51, 63)",
-        "margin": "0 0.125rem",
-        "padding": "0.4375rem 0.625rem",
+        "display": "inline-block",
+        "color": "black",
+        "padding": "0.2rem 0.725rem",
+        "font-size": "14px"
     },
     "active": {
-        "background-color": "rgba(105, 114, 255, 0.25)",
-    },
-    "hover": {
-        "background-color": "rgba(255, 255, 255, 0.35)",
+        "background-color": "mediumpurple",
+        "color": "black",
+        "font-weight": "normal",
+        "padding": "14px"
     }
 }
-
 page = st_navbar(pages, styles=styles)
 
 if page == 'Home':
     Home.Home().app()
-elif page == "Project1":
+elif page == 'Project1':
     Project1.Project1().app()
-elif page == "Project2":
+elif page == 'Project2':
     Project2.Project2().app()
-elif page == "Project3":
+elif page == 'Project3':
     Project3.Project3().app()
 else:
     Home.Home().app()
